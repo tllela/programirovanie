93bcabedaf78266465529f3fe55bee2c0d770758
tllela <talibovaleylaleyla@gmail.com>
2025-05-07 10:40:24 +0300

    Update goot.py

--- goot.py
+++ goot.py
@@ -1,14 +1,33 @@
 import os
 import time
 
-hosts= ["8.8.8.8","1.1.1.1", "192.168.1.1"]
+import csv
+import datetime
+
+
+hosts = ["8.8.8.8","1.1.1.1", "192.168.1.1" ]
+
+with open("ping_loh.csv","w",newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Time","Status"])
+    
 while True:
     print("kättesadavuse kontroll")
+    now = datetime.datetime.now()
+    result = "OK"
     for elem in hosts:
-        response = os.system(f"ping -n 1 {elem} > n")
+        response = os.system(f"ping -n 1 {elem} > null")
         if response == 0:
-            print(elem, "kätesadavalt")
+            result = "OK"
+            print(elem, "kättesadavalt")
         else:
-            print(elem, "ei ole kätesadavalt")
+            result = "Fail"
+            print(elem, "ei ole kättesadavalt")
+            
+            
+        with open("ping_loh.csv","w",newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([now,result])
+            
         print("-"*30)
         time.sleep(5)
