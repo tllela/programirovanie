041e72b42237dd269ebaeeae7afdf9df1f2ffc25
tllela <talibovaleylaleyla@gmail.com>
2025-05-21 09:53:44 +0300

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,21 @@
+class MurderStatsManager:
+    def __init__(self):
+        self.data = {}
+
+    def add_data(self, continent, country, city, count):
+        if continent not in self.data:
+            self.data[continent] = {}
+        if country not in self.data[continent]:
+            self.data[continent][country] = {}
+        self.data[continent][country][city] = count
+
+    def get_stats(self):
+        return self.data
+
+if __name__ == "__main__":
+    manager = MurderStatsManager()
+    manager.add_data("Europe", "Estonia", "Tallinn", 5)
+    manager.add_data("Europe", "Finland", "Helsinki", 3)
+    manager.add_data("Asia", "Japan", "Tokyo", 8)
+
+    print(manager.get_stats())
